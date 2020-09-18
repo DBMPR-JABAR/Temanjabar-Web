49a3547f6c2cae0100551c2be05f8a835c6b4fd5
priyayidimas <priyayidimas@upi.edu>
2020-09-18 15:14:01 +0700

    Add Assets to Project

---
+++ resources/views/admin/sidenav.blade.php
@@ -0,0 +1,160 @@
+<nav class="pcoded-navbar">
+    <div class="pcoded-inner-navbar main-menu">
+        <div class="pcoded-navigatio-lavel">Dashboard Analysis</div>
+     
+
+        <ul class="pcoded-item pcoded-left-item">
+            <li class="pcoded-hasmenu">
+                <a href="javascript:void(0)">
+                    <span class="pcoded-micon"><i class="ti-bar-chart"></i></span>
+                    <span class="pcoded-mtext">Monitoring</span>
+                </a>
+                <ul class="pcoded-submenu">
+                    <li class=" ">
+                        <a href="progress-pekerjaan.html">
+                            <span class="pcoded-mtext">Map Progress Pekerjaan</span>
+                        </a>
+                    </li>
+                    <li class=" ">
+                        <a href="supervisi-pekerjaan.html">
+                            <span class="pcoded-mtext">Supervisi Pekerjaan</span>
+                        </a>
+                    </li>
+                    <li class=" ">
+                        <a href="laporan-kerusakan.html">
+                            <span class="pcoded-mtext">Laporan Kerusakan</span>
+                        </a>
+                    </li>
+                    <li class=" ">
+                        <a href="realisasi-keuangan.html">
+                            <span class="pcoded-mtext">Anggaran & Realisasi Keuangan</span>
+                        </a>
+                    </li>
+                    <li class="">
+                        <a href="audit-keuangan.html" class="disabled">
+                            <span class="pcoded-mtext">Audit Keuangan (Pending)</span>
+                        </a>
+                    </li>
+                </ul>
+            </li>
+            <li class="pcoded-hasmenu">
+                <a href="javascript:void(0)">
+                    <span class="pcoded-micon"><i class="ti-stats-up"></i></span>
+                    <span class="pcoded-mtext">Rekomendasi</span>
+                </a>
+                <ul class="pcoded-submenu">
+                    <li class=" ">
+                        <a href="rekomendasi-kontraktor.html">
+                            <span class="pcoded-mtext">Kontraktor Terbaik</span>
+                        </a>
+                    </li>
+                    <li class=" ">
+                        <a href="rekomendasi-konsultan.html">
+                            <span class="pcoded-mtext">Konsultan Terbaik</span>
+                        </a>
+                    </li>
+                    <li class=" ">
+                        <a href="rekomendasi-perbaikan.html">
+                            <span class="pcoded-mtext">Perbaikan Infrastruktur</span>
+                        </a>
+                    </li>
+                </ul>
+            </li>
+        </ul>
+        <hr>
+        <div class="pcoded-navigatio-lavel">Implementation Only</div>
+        <div class="pcoded-navigatio-lavel">Data Utama</div>
+        <ul class="pcoded-item pcoded-left-item">
+            <li class="pcoded-hasmenu">
+                <a href="javascript:void(0)">
+                    <span class="pcoded-micon"><i class="feather icon-package"></i></span>
+                    <span class="pcoded-mtext">Data Utama</span>
+                </a>
+                <ul class="pcoded-submenu">
+                    <li class=" ">
+                        <a href="">
+                            <span class="pcoded-mtext">Kontraktor</span>
+                        </a>
+                    </li>
+                    <li class=" ">
+                        <a href="">
+                            <span class="pcoded-mtext">Konsultan</span>
+                        </a>
+                    </li>
+                    <li class=" ">
+                        <a href="">
+                            <span class="pcoded-mtext">PPK</span>
+                        </a>
+                    </li>
+                    <li class=" ">
+                        <a href="">
+                            <span class="pcoded-mtext">Jenis Pekerjaan</span>
+                        </a>
+                    </li>
+                    <li class=" ">
+                        <a href="">
+                            <span class="pcoded-mtext">Pengguna</span>
+                        </a>
+                    </li>
+
+                </ul>
+            </li>
+            <li class="pcoded-hasmenu">
+                <a href="javascript:void(0)">
+                    <span class="pcoded-micon"><i class="feather icon-edit-1"></i></span>
+                    <span class="pcoded-mtext">Input Data</span>
+                </a>
+                <ul class="pcoded-submenu">
+                    <li class=" ">
+                        <a href="">
+                            <span class="pcoded-mtext">Data Umum</span>
+                        </a>
+                    </li>
+                    <li class=" ">
+                        <a href="">
+                            <span class="pcoded-mtext">Jadwal Pekerjaan</span>
+                        </a>
+                    </li>
+                    <li class=" ">
+                        <a href="">
+                            <span class="pcoded-mtext">Permintaan</span>
+                        </a>
+                    </li>
+                    <li class=" ">
+                        <a href="">
+                            <span class="pcoded-mtext">Laporan Harian</span>
+                        </a>
+                    </li>
+                </ul>
+            </li>
+            <li class="pcoded-hasmenu">
+                <a href="javascript:void(0)">
+                    <span class="pcoded-micon"><i class="feather icon-download-cloud"></i></span>
+                    <span class="pcoded-mtext">Pusat Unduhan</span>
+                </a>
+                <ul class="pcoded-submenu">
+                    <li class=" ">
+                        <a href="">
+                            <span class="pcoded-mtext">Data Kontrak</span>
+                        </a>
+                    </li>
+                </ul>
+            </li>
+        </ul>
+        <div class="pcoded-navigatio-lavel">Cetak Laporan</div>
+        <ul class="pcoded-item pcoded-left-item">
+            <li class="">
+                <a href="">
+                    <span class="pcoded-micon"><i class="ti-pulse"></i></span>
+                    <span class="pcoded-mtext">Progress</span>
+                </a>
+            </li>
+            <li class="">
+                <a href="">
+                    <span class="pcoded-micon"><i class="ti-blackboard"></i></span>
+                    <span class="pcoded-mtext">Laporan Pekerjaan</span>
+                </a>
+            </li>
+        </ul>
+    </div>
+</nav>
---
+++ resources/views/admin/t_index.blade.php
@@ -0,0 +1,247 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Temanjabar &middot; @yield('title')</title>
+    <!-- HTML5 Shim and Respond.js IE10 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 10]>
+    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    <!-- Meta -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="description" content="#">
+    <meta name="keywords" content="flat ui, admin Admin , Responsive, Landing, Bootstrap, App, Template, Mobile, iOS, Android, apple, creative app">
+    <meta name="author" content="#">
+    <!-- Favicon icon -->
+    <link rel="icon" href="../images/favicon/favicon.ico" type="image/x-icon">
+    <!-- Google font-->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600" rel="stylesheet">
+    <!-- Required Fremwork -->
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets/vendor/bootstrap/css/bootstrap.min.css') }}">
+    <!-- themify-icons line icon -->
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets/icon/themify-icons/themify-icons.css') }}">
+    <!-- ico font -->
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets/icon/icofont/css/icofont.css') }}">
+    <!-- feather Awesome -->
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets/icon/feather/css/feather.css') }}">
+    <!-- Style.css -->
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/style.css')}} ">
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/jquery.mCustomScrollbar.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/scss/partials/menu/_pcmenu.scss') }}">
+    @yield('head')
+</head>
+
+<body>
+<!-- Pre-loader start -->
+<div class="theme-loader">
+    <div class="ball-scale">
+        <div class='contain'>
+            <div class="ring">
+                <div class="frame"></div>
+            </div>
+            <div class="ring">
+                <div class="frame"></div>
+            </div>
+            <div class="ring">
+                <div class="frame"></div>
+            </div>
+            <div class="ring">
+                <div class="frame"></div>
+            </div>
+            <div class="ring">
+                <div class="frame"></div>
+            </div>
+            <div class="ring">
+                <div class="frame"></div>
+            </div>
+            <div class="ring">
+                <div class="frame"></div>
+            </div>
+            <div class="ring">
+                <div class="frame"></div>
+            </div>
+            <div class="ring">
+                <div class="frame"></div>
+            </div>
+            <div class="ring">
+                <div class="frame"></div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<!-- Pre-loader end -->
+<div id="pcoded" class="pcoded">
+    <div class="pcoded-overlay-box"></div>
+    <div class="pcoded-container navbar-wrapper">
+
+        @include('admin.navbar')
+
+        <div class="pcoded-main-container">
+            <div class="pcoded-wrapper">
+
+                @include('admin.sidenav')
+
+                <!-- TODO: Refactor Content -->
+
+                <div class="pcoded-content">
+                    <div class="pcoded-inner-content">
+                        <!-- Main-body start -->
+                        <div class="main-body">
+                            <div class="page-wrapper">
+                                <!-- Page-header start -->
+                                <div class="page-header">
+                                    <div class="row align-items-end">
+                                        <div class="col-lg-8">
+                                            <div class="page-header-title">
+                                                <div class="d-inline">
+                                                    <h4>Sample Page</h4>
+                                                    <span>lorem ipsum dolor sit amet, consectetur adipisicing elit</span>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="col-lg-4">
+                                            <div class="page-header-breadcrumb">
+                                                <ul class="breadcrumb-title">
+                                                    <li class="breadcrumb-item">
+                                                        <a href="index-1.htm"> <i class="feather icon-home"></i> </a>
+                                                    </li>
+                                                    <li class="breadcrumb-item"><a href="#!">Widget</a> </li>
+                                                </ul>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <!-- Page-header end -->
+
+                                <div class="page-body">
+                                    <div class="row">
+                                        <div class="col-sm-12">
+                                            <div class="card">
+                                                <div class="card-header">
+                                                    <h5>Hello Card</h5>
+                                                    <span>lorem ipsum dolor sit amet, consectetur adipisicing elit</span>
+                                                    <div class="card-header-right">
+                                                        <ul class="list-unstyled card-option">
+                                                            <li><i class="feather icon-maximize full-card"></i></li>
+                                                            <li><i class="feather icon-minus minimize-card"></i></li>
+                                                            <li><i class="feather icon-trash-2 close-card"></i></li>
+                                                        </ul>
+                                                    </div>
+                                                </div>
+                                                <div class="card-block">
+                                                    <p>
+                                                        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                                                        eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                                                        enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                                                        nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                                                        in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                                                        nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                                                        sunt in culpa qui officia deserunt mollit anim id est laborum."
+                                                    </p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div id="styleSelector">
+
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
+    </div>
+</div>
+
+
+<!-- Warning Section Starts -->
+<!-- Older IE warning message -->
+<!--[if lt IE 10]>
+<div class="ie-warning">
+    <h1>Warning!!</h1>
+    <p>You are using an outdated version of Internet Explorer, please upgrade <br/>to any of the following web browsers
+        to access this website.</p>
+    <div class="iew-container">
+        <ul class="iew-download">
+            <li>
+                <a href="http://www.google.com/chrome/">
+                    <img src="../files/assets/images/browser/chrome.png" alt="Chrome">
+                    <div>Chrome</div>
+                </a>
+            </li>
+            <li>
+                <a href="https://www.mozilla.org/en-US/firefox/new/">
+                    <img src="../files/assets/images/browser/firefox.png" alt="Firefox">
+                    <div>Firefox</div>
+                </a>
+            </li>
+            <li>
+                <a href="http://www.opera.com">
+                    <img src="../files/assets/images/browser/opera.png" alt="Opera">
+                    <div>Opera</div>
+                </a>
+            </li>
+            <li>
+                <a href="https://www.apple.com/safari/">
+                    <img src="../files/assets/images/browser/safari.png" alt="Safari">
+                    <div>Safari</div>
+                </a>
+            </li>
+            <li>
+                <a href="http://windows.microsoft.com/en-us/internet-explorer/download-ie">
+                    <img src="../files/assets/images/browser/ie.png" alt="">
+                    <div>IE (9 & above)</div>
+                </a>
+            </li>
+        </ul>
+    </div>
+    <p>Sorry for the inconvenience!</p>
+</div>
+<![endif]-->
+<!-- Warning Section Ends -->
+<!-- Required Jquery -->
+
+
+<script type="text/javascript" src="{{ asset('assets/vendor/jquery/js/jquery.min.js') }}"></script>
+<script type="text/javascript" src="{{ asset('assets/vendor/jquery-ui/js/jquery-ui.min.js') }}"></script>
+<script type="text/javascript" src="{{ asset('assets/vendor/popper.js/js/popper.min.js') }}"></script>
+<script type="text/javascript" src="{{ asset('assets/vendor/bootstrap/js/bootstrap.min.js') }}"></script>
+<!-- jquery slimscroll js -->
+<script type="text/javascript" src="{{ asset('assets/vendor/jquery-slimscroll/js/jquery.slimscroll.js') }}"></script>
+<!-- modernizr js -->
+<script type="text/javascript" src="{{ asset('assets/vendor/modernizr/js/modernizr.js') }}"></script>
+<script type="text/javascript" src="{{ asset('assets/vendor/modernizr/js/css-scrollbars.js') }}"></script>
+
+<!-- i18next.min.js -->
+<script type="text/javascript" src="{{ asset('assets/vendor/i18next/js/i18next.min.js') }}"></script>
+<script type="text/javascript" src="{{ asset('assets/vendor/i18next-xhr-backend/js/i18nextXHRBackend.min.js') }}"></script>
+<script type="text/javascript" src="{{ asset('assets/vendor/i18next-browser-languagedetector/js/i18nextBrowserLanguageDetector.min.js') }}"></script>
+<script type="text/javascript" src="{{ asset('assets/vendor/jquery-i18next/js/jquery-i18next.min.js') }}"></script>
+<script src="{{ asset('assets/js/pcoded.min.js') }}"></script>
+<script src="{{ asset('assets/js/vartical-layout.min.js') }}"></script>
+<script src="{{ asset('assets/js/jquery.mCustomScrollbar.concat.min.js') }}"></script>
+    <!-- Custom js -->
+    <script type="text/javascript" src="{{ asset('assets/js/script.min.js') }}"></script>
+
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async="" src="https://www.googletagmanager.com/gtag/js?id=UA-23581568-13"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-23581568-13');
+</script>
+</body>
+
+</html>
