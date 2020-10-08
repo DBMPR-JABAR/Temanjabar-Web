3c0e31c2df8565723ba73389aa4e44eaac96759a
rahmannurhidayat022 <rahmannurhidayat022@gmail.com>
2020-10-08 07:50:19 +0700

    memperbaiki halaman login ke dashboard

--- resources/views/landing/login.blade.php
+++ resources/views/landing/login.blade.php
@@ -5,12 +5,12 @@
 <section id="sign-in" class="bglight position-relative padding">
   <div class="container">
       <div class="row">
-          <div class="col-lg-6 pr-0">
-              <div class=" image login-image h-100">
-                  <img src="{{ asset('assets/images/slider/hero01.jpg') }}" alt="" class="w-100 h-100">
+          <div class="col-lg-6 col-md-6 rp-0">
+              <div class="image login-image h-100">
+                  <img src="{{ asset('assets/images/slider/hero01.jpg') }}" alt="" class="w-100 h-100" style="object-fit: cover;">
               </div>
           </div>
-         <div class="col-lg-6 pl-0 col-md-8 col-sm-10 whitebox">
+         <div class="col-md-6 col-md-6 col-sm-12 whitebox">
             <div class="widget logincontainer">
                <h3 class="darkcolor bottom35">Login </h3>
                @if (Session::has('msg'))
@@ -42,8 +42,7 @@
                      </div>
                      <div class="col-md-12 col-sm-12">
                         <div class="form-group bottom35">
-
-                           <div class="form-check text-left">
+                           <div class="form-check text-right">
                               <input class="form-check-input" checked type="checkbox" value="" id="rememberMe">
                               <label class="form-check-label" for="rememberMe">
                                     Keep Me Signed In
