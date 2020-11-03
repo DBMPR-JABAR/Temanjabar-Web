8e453fd07c5cacee222b7ff8a58a5d2927ded0d9
priyayidimas <priyayidimas@upi.edu>
2020-11-03 08:19:51 +0700

    Reset Password

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -59,7 +59,7 @@ class AuthController extends Controller
 
             if($validator->fails()){
                 $this->response['data']['error'] = $validator->errors();
-                return response()->json($this->response, 400);
+                return response()->json($this->response, 200);
             }
 
             // Create User Data
@@ -95,7 +95,7 @@ class AuthController extends Controller
         }
     }
 
-    public function resetPassword(Request $req)
+    public function resetPasswordMail(Request $req)
     {
         try {
             $validator = Validator::make($req->all(), [
@@ -103,13 +103,57 @@ class AuthController extends Controller
             ]);
             if($validator->fails()){
                 $this->response['data']['error'] = $validator->errors();
-                return response()->json($this->response, 400);
+                return response()->json($this->response, 200);
             }
 
-            // TODO: Send Email
+            $kode_otp = rand(100000, 999999);
+
+            $user = User::where('email',$req->email)->first();
+            $user->kode_otp = $kode_otp;
+            $user->save();
+
+            $data = [
+                'name' => $user->name,
+                'kode_otp' => $kode_otp
+            ];
+            $to_email = $user->email;
+            $to_name = $user->name;
+
+            Mail::send('mail.sendOTP', $data, function($message) use ($to_name, $to_email) {
+                $message->to($to_email, $to_name)->subject('Verifikasi OTP Reset Password Akun Temanjabar');
+
+                $message->from(env('MAIL_USERNAME'),env('MAIL_FROM_NAME'));
+            });
 
             $this->response['status'] = 'success';
-            $this->response['data'] = true;
+            $this->response['data']['kode_otp'] = $kode_otp;
+            $this->response['data']['message'] = "Kode Verifikasi Dikirim";
+
+            return response()->json($this->response,200);
+
+        } catch (\Exception $e) {
+            $this->response['data']['message'] = 'Internal Error';
+            return response()->json($this->response,500);
+        }
+    }
+    public function resetPassword(Request $req)
+    {
+        try {
+            $validator = Validator::make($req->all(), [
+                'email' => 'required|string|email|max:255|exists:users',
+                'password' => 'required|string|min:6',
+            ]);
+            if($validator->fails()){
+                $this->response['data']['error'] = $validator->errors();
+                return response()->json($this->response, 200);
+            }
+
+            $user = User::where('email',$req->email)->first();
+            $user->password = Hash::make($req->get('password'));
+            $user->save();
+
+            $this->response['status'] = 'success';
+            $this->response['data']['message'] = "Berhasil Mengubah Password";
 
             return response()->json($this->response,200);
 
@@ -227,19 +271,19 @@ class AuthController extends Controller
 
             if($validator->fails()){
                 $this->response['data']['error'] = $validator->errors();
-                return response()->json($this->response, 400);
+                return response()->json($this->response, 200);
             }
 
             $kode_otp = rand(100000, 999999);
 
             // Create User Data
-            $user = User::create([
-                'name' => $req->get('name'),
-                'email' => $req->get('email'),
-                'password' => Hash::make($req->get('password')),
-                'role' => 'masyarakat',
-                'kode_otp' => $kode_otp
-            ]);
+            $user = new User;
+            $user->name = $req->get('name');
+            $user->email = $req->get('email');
+            $user->password = Hash::make($req->get('password'));
+            $user->role = 'masyarakat';
+            $user->kode_otp = $kode_otp;
+            $user->save();
 
             // Send Email Verification
             $to_email = $user->email;
