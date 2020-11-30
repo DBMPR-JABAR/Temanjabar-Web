629f39082718463f2ecadfc6268486de9af97cc7
priyayidimas <priyayidimas@upi.edu>
2020-11-30 08:38:38 +0700

    Add Rute Jalanan

--- resources/views/admin/map/map-dashboard-0.blade.php
+++ resources/views/admin/map/map-dashboard-0.blade.php
@@ -88,7 +88,7 @@
           cursor: pointer;
         }
         .form-group {
-            margin-bottom: 1px;
+            margin-bottom: 1px; */
         }
         #back {
             position: absolute;
@@ -180,12 +180,12 @@
             </div>
         </div>
         <form class="py-3">
-            <div class="row">
+            {{-- <div class="row">
                 <div class="col">
                     <button type="button" class="btn btn-sm btn-block btn-secondary clustering">Disable Clustering</button>
                 </div>
             </div>
-            <hr>
+            <hr> --}}
             <div class="form-group">
                 <label for="kegiatan"><i class="feather icon-target text-primary"></i> UPTD</label>
                 <select class="form-control chosen-select chosen-select-uptd" id="uptd" multiple data-placeholder="Pilih UPTD">
@@ -242,7 +242,7 @@
         </form>
     </div>
     <div id="baseMaps" class="bg-white">
-            <div class="row">
+            {{-- <div class="row">
                 <div class="col">
                     <h6>Tipe Maps</h6>
                 </div>
@@ -257,7 +257,7 @@
                     </div>
                 </div>
             </div>
-            <hr>
+            <hr> --}}
             <div class="listMaps">
                 <div class="row mb-4">
                     <div class="col">
@@ -266,56 +266,56 @@
                 </div>
                 <ul class="row">
                     <li>
-                        <button>
+                        <button class="baseMapBtn" data-map="streets">
                             <img _ngcontent-btg-c5="" alt="Rupa Bumi Indonesia" title="Rupa Bumi Indonesia"
                             src="https://portal.ina-sdi.or.id/arcgis/rest/services/RBI/Basemap/MapServer/info/thumbnail">
                         </button>
                     </li>
                     <li>
-                        <button>
+                        <button class="baseMapBtn" data-map="gray">
                             <img _ngcontent-pmm-c5="" alt="Cartodb Light All" title="Cartodb Light All"
                             src="https://satupeta-dev.digitalservice.id/assets/img/basemap-thumbnail/cartodb_light.png">
                         </button>
                     </li>
                     <li>
-                        <button>
-                            <img _ngcontent-vgg-c5="" alt="Cartodb Dark All" title="Cartodb Dark All"
+                        <button class="baseMapBtn" data-map="streets-night-vector">
+                            <img _ngcontent-vgg-c5="" alt="Cartodb Dark All" title="Streets Night Vector"
                             src="https://satupeta-dev.digitalservice.id/assets/img/basemap-thumbnail/cartodb_dark.png">
                         </button>
                     </li>
                     <li>
-                        <button>
+                        <button class="baseMapBtn" data-map="national-geographic">
                             <img _ngcontent-vgg-c5="" alt="National Geographic" title="National Geographic"
                             src="https://js.arcgis.com/4.14/esri/images/basemap/national-geographic.jpg">
                         </button>
                     </li>
                     <li>
-                        <button>
+                        <button class="baseMapBtn" data-map="topo">
                             <img _ngcontent-lqn-c5="" alt="Topographic" title="Topographic"
                             src="https://satupeta-dev.digitalservice.id/assets/img/basemap-thumbnail/topo.png"></button></li>
                         </button>
                     </li>
                     <li>
-                        <button>
+                        <button class="baseMapBtn" data-map="dark-gray">
                             <img _ngcontent-lqn-c5="" alt="Dark Gray" title="Dark Gray"
                             src="https://js.arcgis.com/4.14/esri/images/basemap/dark-gray.jpg">
                         </button>
                     </li>
                     <li>
-                        <button>
+                        <button class="baseMapBtn" data-map="osm">
                             <img _ngcontent-lqn-c5="" alt="Open Street Map" title="Open Street Map"
                             src="https://js.arcgis.com/4.14/esri/images/basemap/osm.jpg">
                         </button>
                     </li>
                     <li>
-                        <button>
-                            <img _ngcontent-lqn-c5="" alt="Imagery" title="Imagery"
+                        <button class="baseMapBtn" data-map="hybrid">
+                            <img _ngcontent-lqn-c5="" alt="hybrid" title="hybrid"
                             src="https://js.arcgis.com/4.14/esri/images/basemap/hybrid.jpg">
                         </button>
                     </li>
                     <li>
-                        <button>
-                            <img _ngcontent-lqn-c5="" alt="Terrain" title="Terrain"
+                        <button class="baseMapBtn" data-map="terrain">
+                            <img _ngcontent-lqn-c5="" alt="terrain" title="terrain"
                             src="https://js.arcgis.com/4.14/esri/images/basemap/terrain.jpg">
                         </button>
                     </li>
@@ -372,9 +372,9 @@
   const fullScreenElemn =  document.querySelector('#fullscreen');
   fullScreenElemn.addEventListener('click', () => {
     toggleFullscreen();
-  });
+  })
 
-// <!-- enable clustering -->
+<!-- enable clustering -->
   const clusteringElmn = document.querySelector('.clustering');
 </script>
 
@@ -496,6 +496,8 @@
             }
         });
 
+        let basemap = "hybrid";
+
         const config = {
             '.chosen-select'           : { width: '100%', padding: '0'},
             '.chosen-select-deselect'  : { allow_single_deselect: true },
@@ -532,15 +534,14 @@
                 "esri/layers/GroupLayer",
                 "esri/tasks/RouteTask",
                 "esri/tasks/support/RouteParameters",
-                "esri/tasks/support/FeatureSet",
-                "esri/layers/FeatureLayer" // dimz-add
+                "esri/tasks/support/FeatureSet"
             ], function (Map, MapView, esriRequest, Point, Graphic, GraphicsLayer,
-                        GroupLayer, RouteTask, RouteParameters, FeatureSet, FeatureLayer) { // dimz-add
+                        GroupLayer, RouteTask, RouteParameters, FeatureSet) {
 
                 // Map Initialization
                 const baseUrl = "{{url('/')}}";
                 const map = new Map({
-                    basemap: "hybrid"
+                    basemap: basemap
                 });
                 const view = new MapView({
                     container: "viewDiv",
@@ -550,15 +551,7 @@
                 });
 
                 // Layering
-
-                // dimz-add
-                // let ruasjalanLayer = new GraphicsLayer();
-                let rutejalanLayer = new FeatureLayer({
-                    // url: "https://satupeta.jabarprov.go.id/arcgis/rest/services/SATUPETA_DBMPR/Jaringan_Jalan_25K/MapServer/2/"
-                    // url: "http://localhost:8080/geoserver/gsr/services/temanjabar/FeatureServer/0/",
-                    url: "http://citylab.itb.ac.id:8080/geoserver/gsr/services/temanjabar/FeatureServer/0/",
-                });
-                // end dimz-add
+                let ruteLayer = new GraphicsLayer();
                 let ruasjalanLayer = new GraphicsLayer();
                 let jembatanLayer = new GraphicsLayer();
                 let pembangunanLayer = new GraphicsLayer();
@@ -585,7 +578,6 @@
                     const data = json.data;
                     if(json.status === "success"){
                         ruasjalanLayer = addRuasJalan(data.ruasjalan, ruasjalanLayer);
-                        rutejalanLayer = addRuteJalan(data.ruasjalan, rutejalanLayer);
                         pembangunanLayer = addPembangunan(data.pembangunan, pembangunanLayer);
                         peningkatanLayer = addPeningkatan(data.peningkatan, peningkatanLayer);
                         rehabilitasiLayer = addRehabilitasi(data.rehabilitasi, rehabilitasiLayer);
@@ -614,10 +606,11 @@
                     console.log(error);
                 });
 
+
+
                 // Creating Group Layer
                 const groupLayer = new GroupLayer();
                 groupLayer.add(jembatanLayer);
-                if ($.inArray('ruasjalan', $('#kegiatan').val()) >= 0 && $('#uptd').val().length != 0) {groupLayer.add(rutejalanLayer);} // dimz-add
                 groupLayer.add(ruasjalanLayer);
                 groupLayer.add(pemeliharaanLayer);
                 groupLayer.add(pembangunanLayer);
@@ -627,10 +620,12 @@
                 groupLayer.add(kemantapanjalanLayer);
                 map.add(groupLayer);
 
-                $("#basemap").change(function(){
-                    const basemap = this.value;
+
+                $(".baseMapBtn").click(function(event){
+                    basemap = $(this).data('map');
                     map.basemap = basemap;
                 });
+
                 $("#zoom").change(function(){
                     const zoom = this.value;
                     view.zoom = zoom;
@@ -945,6 +940,7 @@
                         width: "28px",
                         height: "28px"
                     };
+                    console.log(symbol.url);
                     const popupTemplate = {
                         title: "{RUAS_JALAN}",
                         content: [{
@@ -1003,8 +999,7 @@
                     });
                     return layer;
                 }
-                // dimz-edit
-                function addRuasJalan(ruasjalan, layer){
+                function addRuasJalan(ruasjalan, layer) {
                     const symbol = {
                         type: "picture-marker",  // autocasts as new PictureMarkerSymbol()
                         url: baseUrl + "/assets/images/marker/jalan.png",
@@ -1033,20 +1028,15 @@
                                 label: "Longitude 1"
                             },
                             {
-                                fieldName: "UPTD",
-                                label: "UPTD"
-                            },
-                            {
                                 fieldName: "SUP",
                                 label: "SUP"
+                            },
+                            {
+                                fieldName: "UPTD",
+                                label: "UPTD"
                             }
-                        ]}],
-                        expressionInfos: [{
-                            name: "pjg_km",
-                            title: "Panjang Ruas (KM)",
-                            expression: "Round($feature.pjg_ruas_m / 1000, 2)"
-                        }]
-                    };
+                        ]}
+                    ]};
                     ruasjalan.forEach(item => {
                         let point = new Point(item.LONG_AWAL, item.LAT_AWAL);
                         layer.graphics.add(new Graphic({
@@ -1064,72 +1054,6 @@
                             popupTemplate: popupTemplate
                         }));
                     });
-                    return layer;
-                }
-                function addRuteJalan(rutejalan, layer) {
-                    const popupTemplate = {
-                        title: "{nm_ruas}",
-                        content: [{
-                        type: "fields",
-                        fieldInfos: [
-                            {
-                                fieldName: "LAT_AWAL",
-                                label: "Latitude 0"
-                            },
-                            {
-                                fieldName: "LONG_AWAL",
-                                label: "Longitude 0"
-                            },
-                            {
-                                fieldName: "LAT_AKHIR",
-                                label: "Latitude 1"
-                            },
-                            {
-                                fieldName: "LONG_AKHIR",
-                                label: "Longitude 1"
-                            },
-                            {
-                                fieldName: "kab_kota",
-                                label: "Kab/Kota"
-                            },
-                            {
-                                fieldName: "wil_uptd",
-                                label: "UPTD"
-                            },
-                            {
-                                fieldName: "nm_sppjj",
-                                label: "SUP"
-                            },
-                            {
-                                // fieldName: "pjg_ruas_m",
-                                // label: "Panjang Ruas (m)"
-                                fieldName: "expression/pjg_km",
-                            }
-                        ]}],
-                        expressionInfos: [{
-                            name: "pjg_km",
-                            title: "Panjang Ruas (KM)",
-                            expression: "Round($feature.pjg_ruas_m / 1000, 2)"
-                        }]
-                    };
-
-                    // dimz-add
-                    if ($.inArray('ruasjalan', $('#kegiatan').val()) >= 0 && $('#uptd').val().length != 0) {
-                        var uptdSel = $('#uptd').val();
-                        var whereUptd = 'uptd=' + uptdSel.shift().charAt(4);
-                        $.each(uptdSel, function(idx, elem) {
-                            whereUptd = whereUptd + ' OR uptd=' + elem.charAt(4);
-                        });
-                        layer.popupTemplate = popupTemplate;
-                        layer.definitionExpression = whereUptd;
-                    } else {
-                        layer.definitionExpression= '0=1';
-                    }
-
-                    // require(["esri/config"], function (esriConfig) {
-                    //     esriConfig.request.corsEnabledServers.push("localhost:8080");
-                    // });
-                    // end dimz-add
                     return layer;
                 }
                 function addProgressGroup(progress) {
@@ -1553,7 +1477,7 @@
 
         }
 
-        getMapData("","");
+        getMapData("","",basemap);
     });
 </script>
 {{-- <script>
