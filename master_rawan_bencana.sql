7ba234fc5fb0085f9276bfc88bf3ed4f230dc320
priyayidimas <priyayidimas@upi.edu>
2020-12-03 11:17:15 +0700

    db master

--- master_rawan_bencana.sql
+++ master_rawan_bencana.sql
@@ -11,39 +11,34 @@
  Target Server Version : 100414
  File Encoding         : 65001
 
- Date: 02/12/2020 20:35:05
+ Date: 02/12/2020 20:44:24
 */
 
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
 
 -- ----------------------------
--- Table structure for master_ruas_jalan
+-- Table structure for master_rawan_bencana
 -- ----------------------------
-DROP TABLE IF EXISTS `master_ruas_jalan`;
-CREATE TABLE `master_ruas_jalan`  (
+DROP TABLE IF EXISTS `master_rawan_bencana`;
+CREATE TABLE `master_rawan_bencana`  (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `id_ruas_jalan` char(6) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `nama_ruas_jalan` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `sup` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `no_ruas` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `status` varchar(5) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `ruas_jalan` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `lokasi` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `panjang` decimal(10, 2) NOT NULL,
-  `sta_awal` float NOT NULL,
-  `sta_akhir` float NOT NULL,
-  `lat_awal` float NOT NULL,
-  `long_awal` float NOT NULL,
-  `lat_akhir` float NOT NULL,
-  `long_akhir` float NOT NULL,
-  `uptd_id` int(11) NULL DEFAULT NULL,
-  `created_date` datetime(0) NULL DEFAULT NULL,
+  `daerah` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `keterangan` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `uptd_id` int(11) NOT NULL,
+  `created_at` timestamp(0) NULL DEFAULT NULL,
+  `updated_at` timestamp(0) NULL DEFAULT NULL,
   `created_by` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
-  `updated_date` datetime(0) NULL DEFAULT NULL,
   `updated_by` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
--- Records of master_ruas_jalan
+-- Records of master_rawan_bencana
 -- ----------------------------
 
 SET FOREIGN_KEY_CHECKS = 1;
