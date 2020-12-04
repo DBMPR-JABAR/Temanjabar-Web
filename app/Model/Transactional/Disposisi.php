cbce6ba0544130da9ea8ab0305206b88d1a0c3f8
zulkifli <izqfly@gmail.com>
2020-12-04 17:12:09 +0700

    add disposisi

---
+++ app/Model/Transactional/Disposisi.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Model\Transactional;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Disposisi extends Model
+{
+    protected $table = "disposisi";
+}
---
+++ app/Model/Transactional/DisposisiPenanggungJawab.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Model\Transactional;
+
+use Illuminate\Database\Eloquent\Model;
+
+class DisposisiPenanggungJawab extends Model
+{
+    protected $table = "disposisi_penanggung_jawab";
+}
