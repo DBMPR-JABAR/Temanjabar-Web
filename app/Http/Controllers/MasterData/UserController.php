b5486e360adeeeed1cee2d4a1c991d8fd384c5fc
fakhriabdullah <fakhri.abdullah9496@gmail.com>
2020-12-16 17:50:37 +0700

    update user

--- app/Http/Controllers/MasterData/UserController.php
+++ app/Http/Controllers/MasterData/UserController.php
@@ -8,6 +8,9 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Hash;
+
 
 class UserController extends Controller
 {
