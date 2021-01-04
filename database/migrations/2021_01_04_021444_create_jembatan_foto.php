871e4f5284ba15620ca8a5363cf30af388039849
fakhriabdullah <fakhri.abdullah9496@gmail.com>
2021-01-04 09:20:30 +0700

    update database jembatan

---
+++ database/migrations/2021_01_04_021444_create_jembatan_foto.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateJembatanFoto extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('master_jembatan_foto', function (Blueprint $table) {
+            $table->id();
+            $table->integer('id_jembatan')->unsigned();
+            $table->string('nama', 100);
+            $table->text('foto');
+            $table->foreign('id_jembatan')->references('id')->on('master_jembatan')->onUpdate('cascade')
+                ->onDelete('cascade');;
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('jembatan_foto');
+    }
+}
