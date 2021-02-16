e3cc4e228e4bee48e9acb346d3dc7eb3b5dc7606
firmanmardiyanto <firman.mardiyanto@gmail.com>
2021-02-16 11:31:01 +0700

    fix exist

--- app/Http/Controllers/InputData/ProgressPekerjaanController.php
+++ app/Http/Controllers/InputData/ProgressPekerjaanController.php
@@ -23,88 +23,88 @@ class ProgressPekerjaanController extends Controller
         $response = [
             'status' => 'false',
             'data' => []
-        ]; 
+        ];
         $pekerjaan = new ProgressPekerjaan();
-        if(Auth::user()->internalRole->uptd){
-            $uptd_id = str_replace('uptd','',Auth::user()->internalRole->uptd);
-            $laporan = $pekerjaan->where('UPTD',$uptd_id);
+        if (Auth::user()->internalRole->uptd) {
+            $uptd_id = str_replace('uptd', '', Auth::user()->internalRole->uptd);
+            $laporan = $pekerjaan->where('UPTD', $uptd_id);
         }
         $pekerjaan = $pekerjaan->get();
         return view('admin.input.progresskerja.index', compact('pekerjaan'));
     }
 
 
-     public function json(){
+    public function json()
+    {
         $progress = DB::table('progress_mingguan');
         if (Auth::user()->internalRole->uptd) {
-            $progress = $progress->where('uptd_id',Auth::user()->internalRole->uptd);
+            $progress = $progress->where('uptd_id', Auth::user()->internalRole->uptd);
         }
         $progress = $progress->get();
         return Datatables::of($progress)
-                ->addIndexColumn()
-                ->addColumn('rencana_temp', function($row){
-                    return $row->rencana.'<br>'.$row->realisasi.'<br>'.$row->deviasi;
-                })
-                ->addColumn('waktu_temp', function($row){
-                    return $row->waktu_kontrak.'<br>'.$row->terpakai.'<br>'.$row->sisa.'<br>'.$row->prosentase;
-                })
-                ->addColumn('nilai_kontrak', function($row){
-                    return number_format($row->nilai_kontrak,2);
-                })
-                ->addColumn('keuangan', function($row){
-                    return $row->bayar.'<br>'.number_format($row->bayar,2);
-                })
-                ->addColumn('action', function($row){
-                   $html = '<div class="btn-group " role="group" data-placement="top" title="" data-original-title=".btn-xlg">';
-                   
-                    if (hasAccess(Auth::user()->internal_role_id, "Progress Kerja", "Update")){
-                        $html.='<a href="'. route('editDataProgress',$row->id).'"><button data-toggle="tooltip" title="Edit" class="btn btn-primary btn-sm waves-effect waves-light"><i class="icofont icofont-pencil"></i></button></a>';
-                        }
-                    if (hasAccess(Auth::user()->internal_role_id, "Progress Kerja", "Delete")){
-                        $html.='<a href="#delModal" data-id="'.$row->id.'" data-toggle="modal"><button data-toggle="tooltip" title="Hapus" class="btn btn-danger btn-sm waves-effect waves-light"><i class="icofont icofont-trash"></i></button></a>';
-                    }
-                    $html.='</div>';
-                    return $html;
+            ->addIndexColumn()
+            ->addColumn('rencana_temp', function ($row) {
+                return $row->rencana . '<br>' . $row->realisasi . '<br>' . $row->deviasi;
+            })
+            ->addColumn('waktu_temp', function ($row) {
+                return $row->waktu_kontrak . '<br>' . $row->terpakai . '<br>' . $row->sisa . '<br>' . $row->prosentase;
+            })
+            ->addColumn('nilai_kontrak', function ($row) {
+                return number_format($row->nilai_kontrak, 2);
+            })
+            ->addColumn('keuangan', function ($row) {
+                return $row->bayar . '<br>' . number_format($row->bayar, 2);
+            })
+            ->addColumn('action', function ($row) {
+                $html = '<div class="btn-group " role="group" data-placement="top" title="" data-original-title=".btn-xlg">';
 
-                })
-                ->make(true);
+                if (hasAccess(Auth::user()->internal_role_id, "Progress Kerja", "Update")) {
+                    $html .= '<a href="' . route('editDataProgress', $row->id) . '"><button data-toggle="tooltip" title="Edit" class="btn btn-primary btn-sm waves-effect waves-light"><i class="icofont icofont-pencil"></i></button></a>';
+                }
+                if (hasAccess(Auth::user()->internal_role_id, "Progress Kerja", "Delete")) {
+                    $html .= '<a href="#delModal" data-id="' . $row->id . '" data-toggle="modal"><button data-toggle="tooltip" title="Hapus" class="btn btn-danger btn-sm waves-effect waves-light"><i class="icofont icofont-trash"></i></button></a>';
+                }
+                $html .= '</div>';
+                return $html;
+            })
+            ->make(true);
     }
 
 
-     public function getDataProgress()
+    public function getDataProgress()
     {
         $pekerjaan = DB::table('progress_mingguan');
         // $pekerjaan = $pekerjaan->leftJoin('master_ruas_jalan', 'master_ruas_jalan.id', '=', 'progress_mingguan.ruas_jalan')->select('progress_mingguan.*', 'master_ruas_jalan.nama_ruas_jalan');
         // print_r(Auth::user()->internalRole->uptd);
         if (Auth::user()->internalRole->uptd) {
-            $pekerjaan = $pekerjaan->where('uptd_id',Auth::user()->internalRole->uptd);
+            $pekerjaan = $pekerjaan->where('uptd_id', Auth::user()->internalRole->uptd);
         }
         $pekerjaan = $pekerjaan->get();
 
         $ruas_jalan = DB::table('master_ruas_jalan');
-         if (Auth::user()->internalRole->uptd) {
-            $ruas_jalan = $ruas_jalan->where('uptd_id',Auth::user()->internalRole->uptd);
+        if (Auth::user()->internalRole->uptd) {
+            $ruas_jalan = $ruas_jalan->where('uptd_id', Auth::user()->internalRole->uptd);
         }
         $ruas_jalan = $ruas_jalan->get();
 
         $sup = DB::table('utils_sup');
-         if (Auth::user()->internalRole->uptd) {
-            $sup = $sup->where('uptd_id',Auth::user()->internalRole->uptd);
+        if (Auth::user()->internalRole->uptd) {
+            $sup = $sup->where('uptd_id', Auth::user()->internalRole->uptd);
         }
         $sup = $sup->get();
 
-        $paket =array();
-        $penyedia =array();
+        $paket = array();
+        $penyedia = array();
         $data2 = DB::table('pembangunan');
         if (Auth::user()->internalRole->uptd) {
-            $data2 = $data2->where('uptd_id',Auth::user()->internalRole->uptd);
+            $data2 = $data2->where('uptd_id', Auth::user()->internalRole->uptd);
         }
         $data2 = $data2->get();
-        foreach ($data2 as $val){
-            if($val->nama_paket!=''){
+        foreach ($data2 as $val) {
+            if ($val->nama_paket != '') {
                 array_push($paket, $val->nama_paket);
             }
-            if($val->penyedia_jasa!=''){
+            if ($val->penyedia_jasa != '') {
                 array_push($penyedia, $val->penyedia_jasa);
             }
         }
@@ -112,42 +112,43 @@ class ProgressPekerjaanController extends Controller
         $jenis = DB::table('utils_jenis_pekerjaan');
         $jenis = $jenis->get();
         $uptd = DB::table('landing_uptd')->get();
-        return view('admin.input.progresskerja.index',compact('jenis','pekerjaan','ruas_jalan','sup','uptd','penyedia','paket'));
+        return view('admin.input.progresskerja.index', compact('jenis', 'pekerjaan', 'ruas_jalan', 'sup', 'uptd', 'penyedia', 'paket'));
     }
-   
+
     public function editDataProgress($id)
     {
-        $progress = DB::table('progress_mingguan')->where(array('id'=>$id));
+        $progressKerja = DB::table('progress_mingguan')->where(array('id' => $id));
         if (Auth::user()->internalRole->uptd) {
-            $progress = $progress->where('uptd_id',Auth::user()->internalRole->uptd);
+            $progressKerja = $progressKerja->where('uptd_id', Auth::user()->internalRole->uptd);
         }
-        $progress = $progress->first();
+        $progressKerja = $progressKerja->first();
 
+        //dd($progress->id);
 
         $ruas_jalan = DB::table('master_ruas_jalan');
-         if (Auth::user()->internalRole->uptd) {
-            $ruas_jalan = $ruas_jalan->where('uptd_id',Auth::user()->internalRole->uptd);
+        if (Auth::user()->internalRole->uptd) {
+            $ruas_jalan = $ruas_jalan->where('uptd_id', Auth::user()->internalRole->uptd);
         }
         $ruas_jalan = $ruas_jalan->get();
 
         $sup = DB::table('utils_sup');
-         if (Auth::user()->internalRole->uptd) {
-            $sup = $sup->where('uptd_id',Auth::user()->internalRole->uptd);
+        if (Auth::user()->internalRole->uptd) {
+            $sup = $sup->where('uptd_id', Auth::user()->internalRole->uptd);
         }
         $sup = $sup->get();
 
-        $paket =array();
-        $penyedia =array();
+        $paket = array();
+        $penyedia = array();
         $data2 = DB::table('pembangunan');
         if (Auth::user()->internalRole->uptd) {
-            $data2 = $data2->where('uptd_id',Auth::user()->internalRole->uptd);
+            $data2 = $data2->where('uptd_id', Auth::user()->internalRole->uptd);
         }
         $data2 = $data2->get();
-        foreach ($data2 as $val){
-            if($val->nama_paket!=''){
+        foreach ($data2 as $val) {
+            if ($val->nama_paket != '') {
                 array_push($paket, $val->nama_paket);
             }
-            if($val->penyedia_jasa!=''){
+            if ($val->penyedia_jasa != '') {
                 array_push($penyedia, $val->penyedia_jasa);
             }
         }
@@ -156,74 +157,74 @@ class ProgressPekerjaanController extends Controller
         $jenis = $jenis->get();
 
         $uptd = DB::table('landing_uptd')->get();
-        return view('admin.input.progresskerja.edit',compact('jenis','progress','ruas_jalan','sup','uptd','penyedia','paket'));
+        return view('admin.input.progresskerja.edit', compact('progressKerja','jenis', 'ruas_jalan', 'sup', 'uptd', 'penyedia', 'paket'));
     }
 
     public function createDataProgress(Request $req)
     {
         $progress = $req->except(['_token']);
         unset($req->_token);
-        $progress['deviasi']=0;
-        $progress['bayar1']=0;
-        $progress['sisa']=0;
-        $progress['prosentase']=0;
-        $progress['kategori']=null;
-        $progress['status']=null;
+        $progress['deviasi'] = 0;
+        $progress['bayar1'] = 0;
+        $progress['sisa'] = 0;
+        $progress['prosentase'] = 0;
+        $progress['kategori'] = null;
+        $progress['status'] = null;
         // $progress['slug'] = Str::slug($req->nama, '');
-        if($req->foto != null){
-            $path = Str::snake(date("YmdHis").' '.$req->foto->getClientOriginalName());
-            $req->foto->storeAs('public/progresskerja/',$path);
-            $progress ['foto'] = $path;
+        if ($req->foto != null) {
+            $path = Str::snake(date("YmdHis") . ' ' . $req->foto->getClientOriginalName());
+            $req->foto->storeAs('public/progresskerja/', $path);
+            $progress['foto'] = $path;
         }
-        if($req->video != null){
-            $path = Str::snake(date("YmdHis").' '.$req->video->getClientOriginalName());
-            $req->video->storeAs('public/progresskerja/',$path);
-            $progress ['video'] = $path;
+        if ($req->video != null) {
+            $path = Str::snake(date("YmdHis") . ' ' . $req->video->getClientOriginalName());
+            $req->video->storeAs('public/progresskerja/', $path);
+            $progress['video'] = $path;
         }
 
         DB::table('progress_mingguan')->insert($progress);
 
         $color = "success";
         $msg = "Berhasil Menambah Data Progress Pekerjaan";
-        return back()->with(compact('color','msg'));
+        return back()->with(compact('color', 'msg'));
     }
     public function updateDataProgress(Request $req)
     {
-        $progress = $req->except('_token','id');
-        $old = DB::table('progress_mingguan')->where('id',$req->id)->first();
-        if($req->foto != null){
-            $old->foto ?? Storage::delete('public/progresskerja/'.$old->foto);
+        $progress = $req->except('_token', 'id');
+        $old = DB::table('progress_mingguan')->where('id', $req->id)->first();
+        if ($req->foto != null) {
+            $old->foto ?? Storage::delete('public/progresskerja/' . $old->foto);
 
-            $path = Str::snake(date("YmdHis").' '.$req->foto->getClientOriginalName());
-            $req->foto->storeAs('public/progresskerja/',$path);
-            $progress ['foto'] = $path;
+            $path = Str::snake(date("YmdHis") . ' ' . $req->foto->getClientOriginalName());
+            $req->foto->storeAs('public/progresskerja/', $path);
+            $progress['foto'] = $path;
         }
 
-        if($req->video != null){
-            $old->video ?? Storage::delete('public/progresskerja/'.$old->video);
+        if ($req->video != null) {
+            $old->video ?? Storage::delete('public/progresskerja/' . $old->video);
 
-            $path = Str::snake(date("YmdHis").' '.$req->video->getClientOriginalName());
-            $req->video->storeAs('public/progresskerja/',$path);
-            $progress ['video'] = $path;
+            $path = Str::snake(date("YmdHis") . ' ' . $req->video->getClientOriginalName());
+            $req->video->storeAs('public/progresskerja/', $path);
+            $progress['video'] = $path;
         }
 
-        DB::table('progress_mingguan')->where('id',$req->id)->update($progress);
+        DB::table('progress_mingguan')->where('id', $req->id)->update($progress);
 
         $color = "success";
         $msg = "Berhasil Mengubah Data Progress Pekerjaan";
-        return redirect(route('getDataProgress'))->with(compact('color','msg'));
+        return redirect(route('getDataProgress'))->with(compact('color', 'msg'));
     }
     public function deleteDataProgress($id)
     {
-        $old = DB::table('progress_mingguan')->where('id',$id)->first();
-        $old->foto ?? Storage::delete('public/progresskerja/'.$old->foto);
-        $old->video ?? Storage::delete('public/progresskerja/'.$old->video);
+        $old = DB::table('progress_mingguan')->where('id', $id)->first();
+        $old->foto ?? Storage::delete('public/progresskerja/' . $old->foto);
+        $old->video ?? Storage::delete('public/progresskerja/' . $old->video);
 
-        $temp = DB::table('progress_mingguan')->where('id',$id);
+        $temp = DB::table('progress_mingguan')->where('id', $id);
         $temp->delete();
 
         $color = "success";
         $msg = "Berhasil Menghapus Data Pekerjaan";
-        return redirect(route('getDataProgress'))->with(compact('color','msg'));
+        return redirect(route('getDataProgress'))->with(compact('color', 'msg'));
     }
 }
--- resources/views/admin/input/progresskerja/edit.blade.php
+++ resources/views/admin/input/progresskerja/edit.blade.php
@@ -39,17 +39,16 @@
                 </div>
             </div>
             <div class="card-block">
-
-                <form action="{{ route('updateDataProgress',$progress->id) }}" method="post" enctype="multipart/form-data">
+                <form action="{{ route('updateDataProgress',$progressKerja->id) }}" method="post" enctype="multipart/form-data">
                     @csrf
 
-                    <input type="hidden" name="id" value="{{$progress->id}}">
+                    <input type="hidden" name="id" value="{{$progressKerja->id}}">
                     <div class="form-group row">
                         <label class="col-md-2 col-form-label">Kegiatan</label>
                         <div class="col-md-10">
-                            <select class="form-control" name="kegiatan" required value="{{$progress->kegiatan}}">
+                            <select class="form-control" name="kegiatan" required value="{{$progressKerja->kegiatan}}">
                                 @foreach ($jenis as $data)
-                                <option value="{{$data->name}}" {{ ( $data->name == $progress->kegiatan) ? 'selected' : ''}}>{{$data->name}}</option>
+                                <option value="{{$data->name}}" {{ ( $data->name == $progressKerja->kegiatan) ? 'selected' : ''}}>{{$data->name}}</option>
                                 @endforeach
                             </select>
                         </div>
@@ -58,16 +57,16 @@
                     <div class="form-group row">
                         <label class="col-md-2 col-form-label">Tanggal</label>
                         <div class="col-md-10">
-                            <input name="tanggal" type="date" class="form-control" required value="{{$progress->tanggal}}">
+                            <input name="tanggal" type="date" class="form-control" required value="{{$progressKerja->tanggal}}">
                         </div>
                     </div>
 
                     <div class="form-group row">
                         <label class="col-md-2 col-form-label">Nama Paket</label>
                         <div class="col-md-10">
-                            <select class="form-control" name="nama_paket" required value="{{$progress->nama_paket}}">
+                            <select class="form-control" name="nama_paket" required value="{{$progressKerja->nama_paket}}">
                                 @foreach ($paket as $data)
-                                <option value="{{$data}}" {{ ( $data == $progress->nama_paket) ? 'selected' : ''}}>{{$data}}</option>
+                                <option value="{{$data}}" {{ ( $data == $progressKerja->nama_paket) ? 'selected' : ''}}>{{$data}}</option>
                                 @endforeach
                             </select>
                         </div>
@@ -75,9 +74,9 @@
                     <div class="form-group row">
                         <label class="col-md-2 col-form-label">Penyedia Jasa</label>
                         <div class="col-md-10">
-                            <select class="form-control" name="penyedia_jasa" required value="{{$progress->penyedia_jasa}}">
+                            <select class="form-control" name="penyedia_jasa" required value="{{$progressKerja->penyedia_jasa}}">
                                 @foreach ($penyedia as $data)
-                                <option value="{{$data}}" {{ ( $data == $progress->penyedia_jasa) ? 'selected' : ''}}>{{$data}}</option>
+                                <option value="{{$data}}" {{ ( $data == $progressKerja->penyedia_jasa) ? 'selected' : ''}}>{{$data}}</option>
                                 @endforeach
                             </select>
                         </div>
@@ -92,7 +91,7 @@
                             <select class="form-control" id="uptd" name="uptd_id" onchange="ubahOption()" required>
                                 <!-- <option value="">Pilih UPTD</option> -->
                                 @foreach ($uptd as $data)
-                                @if($data->id == $progress->uptd_id)
+                                @if($data->id == $progressKerja->uptd_id)
                                 <option value="{{$data->id}}" selected>{{$data->nama}}</option>
                                 @else
                                 <option value="{{$data->id}}">{{$data->nama}}</option>
@@ -106,11 +105,11 @@
                     <div class="form-group row">
                         <label class="col-md-2 col-form-label">Satuan Pelayanan Pengelolaan</label>
                         <div class="col-md-10">
-                            <select class="form-control" name="sup" required value="{{$progress->sup}}">
-                                <option value="<?php echo $progress->sup; ?>"><?php echo $progress->sup; ?></option>
+                            <select class="form-control" name="sup" required value="{{$progressKerja->sup}}">
+                                <option value="<?php echo $progressKerja->sup; ?>"><?php echo $progressKerja->sup; ?></option>
                                 <option disabled></option>
                                 @foreach ($sup as $data)
-                                <option value="{{$data->name}}" {{ ( $data->name == $progress->sup) ? 'selected' : ''}}>{{$data->name}}</option>
+                                <option value="{{$data->name}}" {{ ( $data->name == $progressKerja->sup) ? 'selected' : ''}}>{{$data->name}}</option>
                                 @endforeach
                             </select>
                         </div>
@@ -118,9 +117,9 @@
                     <div class="form-group row">
                         <label class="col-md-2 col-form-label">Ruas Jalan</label>
                         <div class="col-md-10">
-                            <select class="form-control" name="ruas_jalan" required value="{{$progress->ruas_jalan}}">
+                            <select class="form-control" name="ruas_jalan" required value="{{$progressKerja->ruas_jalan}}">
                                 @foreach ($ruas_jalan as $data)
-                                <option value="{{$data->nama_ruas_jalan}}" {{ ( $data->nama_ruas_jalan == $progress->ruas_jalan) ? 'selected' : ''}}>{{$data->nama_ruas_jalan}}</option>
+                                <option value="{{$data->nama_ruas_jalan}}" {{ ( $data->nama_ruas_jalan == $progressKerja->ruas_jalan) ? 'selected' : ''}}>{{$data->nama_ruas_jalan}}</option>
                                 @endforeach
                             </select>
                         </div>
@@ -128,26 +127,26 @@
                     <div class="form-group row">
                         <label class="col-md-2 col-form-label">Jenis Pekerjaan</label>
                         <div class="col-md-10">
-                            <input name="jenis_pekerjaan" type="text" class="form-control" required value="{{$progress->jenis_pekerjaan}}">
+                            <input name="jenis_pekerjaan" type="text" class="form-control" required value="{{$progressKerja->jenis_pekerjaan}}">
                         </div>
                     </div>
                     <div class="form-group row">
                         <label class="col-md-2 col-form-label">Lokasi</label>
                         <div class="col-md-8">
-                            <input name="lokasi" type="text" class="form-control" required value="{{$progress->lokasi}}">
+                            <input name="lokasi" type="text" class="form-control" required value="{{$progressKerja->lokasi}}">
                         </div>
                         <div class="col-md-2"> KM BDG</div>
                     </div>
                     <div class="form-group row">
                         <label class="col-md-2 col-form-label">Koordinat X</label>
                         <div class="col-md-10">
-                            <input name="lat" type="text" class="form-control formatLatLong" required value="{{$progress->lat}}">
+                            <input name="lat" type="text" class="form-control formatLatLong" required value="{{$progressKerja->lat}}">
                         </div>
                     </div>
                     <div class="form-group row">
                         <label class="col-md-2 col-form-label">Koordinat Y</label>
                         <div class="col-md-10">
-                            <input name="lng" type="text" class="form-control formatLatLong" required value="{{$progress->lng}}">
+                            <input name="lng" type="text" class="form-control formatLatLong" required value="{{$progressKerja->lng}}">
                         </div>
                     </div>
 
@@ -155,44 +154,44 @@
                     <div class="form-group row">
                         <label class="col-md-2 col-form-label">Rencana</label>
                         <div class="col-md-10">
-                            <input type="text" name="rencana" class="form-control" value="{{$progress->rencana}}">
+                            <input type="text" name="rencana" class="form-control" value="{{$progressKerja->rencana}}">
                         </div>
                     </div>
                     <div class="form-group row">
                         <label class="col-md-2 col-form-label">Realisasi</label>
                         <div class="col-md-10">
-                            <input type="text" name="realisasi" class="form-control" value="{{$progress->realisasi}}">
+                            <input type="text" name="realisasi" class="form-control" value="{{$progressKerja->realisasi}}">
                         </div>
                     </div>
                     <div class="form-group row">
                         <label class="col-md-2 col-form-label">Waktu Kontrak</label>
                         <div class="col-md-10">
-                            <input type="text" name="waktu_kontrak" class="form-control" value="{{$progress->waktu_kontrak}}">
+                            <input type="text" name="waktu_kontrak" class="form-control" value="{{$progressKerja->waktu_kontrak}}">
                         </div>
                     </div>
                     <div class="form-group row">
                         <label class="col-md-2 col-form-label">Terpakai</label>
                         <div class="col-md-10">
-                            <input type="text" name="terpakai" class="form-control" value="{{$progress->terpakai}}">
+                            <input type="text" name="terpakai" class="form-control" value="{{$progressKerja->terpakai}}">
                         </div>
                     </div>
                     <div class="form-group row">
                         <label class="col-md-2 col-form-label">Nilai Kontrak</label>
                         <div class="col-md-10">
-                            <input type="text" name="nilai_kontrak" class="form-control" value="{{$progress->nilai_kontrak}}">
+                            <input type="text" name="nilai_kontrak" class="form-control" value="{{$progressKerja->nilai_kontrak}}">
                         </div>
                     </div>
                     <div class="form-group row">
                         <label class="col-md-2 col-form-label">Keuangan</label>
                         <div class="col-md-10">
-                            <input type="text" name="bayar" class="form-control" value="{{$progress->bayar}}">
+                            <input type="text" name="bayar" class="form-control" value="{{$progressKerja->bayar}}">
                         </div>
                     </div>
                     <hr>
                     <div class="form-group row">
                         <label class="col-md-2 col-form-label">Foto Dokumentasi</label>
                         <div class="col-md-5">
-                            <img style="max-height: 400px;" class="img-thumbnail rounded mx-auto d-block" src="{{ url('storage/progresskerja/'.$progress->foto) }}" alt="">
+                            <img style="max-height: 400px;" class="img-thumbnail rounded mx-auto d-block" src="{{ url('storage/progresskerja/'.$progressKerja->foto) }}" alt="">
                         </div>
                         <div class="col-md-5">
                             <input name="foto" type="file" class="form-control">
@@ -202,7 +201,7 @@
                         <label class="col-md-2 col-form-label">Video Dokumentasi</label>
                         <div class="col-md-5">
                             <video style="max-height: 400px;" controls class="img-thumbnail rounded mx-auto d-block">
-                                <source src="{{ url('storage/progresskerja/'.$progress->video) }}" type="video/mp4" />
+                                <source src="{{ url('storage/progresskerja/'.$progressKerja->video) }}" type="video/mp4" />
                             </video>
                         </div>
                         <div class="col-md-5">
