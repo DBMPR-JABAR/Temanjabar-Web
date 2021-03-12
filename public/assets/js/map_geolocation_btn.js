bd57a3980d327a19ad37c39f19b47677590fcbfa
Firman Mardiyanto <firman.mardiyanto@gmail.com>
2021-03-12 11:00:44 +0700

    geo

--- public/assets/js/map_geolocation_btn.js
+++ public/assets/js/map_geolocation_btn.js
@@ -4,7 +4,25 @@ const getLocation = ({ idLat, idLong }) => {
     btnGeolocation.html('<i class="fa fa-spinner fa-spin""></i>');
     let location;
 
-    const success = (pos) => {
+    const tryAPIGeolocation = function () {
+        jQuery
+            .post(
+                "https://www.googleapis.com/geolocation/v1/geolocate?key=AIzaSyAKv-benZnSAvT0C8TjaG80pQbybyaz0Q8",
+                function (success) {
+                    apiGeolocationSuccess({
+                        coords: {
+                            latitude: success.location.lat,
+                            longitude: success.location.lng,
+                        },
+                    });
+                }
+            )
+            .fail(function (err) {
+                console.log("API Geolocation error! \n\n" + err);
+            });
+    };
+
+    const apiGeolocationSuccess= (pos) => {
         const crd = pos.coords;
         console.log(crd);
 
@@ -19,9 +37,25 @@ const getLocation = ({ idLat, idLong }) => {
         $(`#${idLong}`).val(crd.longitude).keyup();
     };
 
-    const error = (err) => {
-        console.log(err);
-        // alert('Terjadi kesalahan saat mendapatkan lokasi');
+    const error = (error) => {
+        switch (error.code) {
+            case error.TIMEOUT:
+                console.log("Browser geolocation error !\n\nTimeout.");
+                break;
+            case error.PERMISSION_DENIED:
+                if (
+                    error.message.indexOf("Only secure origins are allowed") ==
+                    0
+                ) {
+                    tryAPIGeolocation();
+                }
+                break;
+            case error.POSITION_UNAVAILABLE:
+                console.log(
+                    "Browser geolocation error !\n\nPosition unavailable."
+                );
+                break;
+        }
     };
 
     const options = {
@@ -29,7 +63,7 @@ const getLocation = ({ idLat, idLong }) => {
         maximumAge: 0,
     };
 
-    location = navigator.geolocation.watchPosition(success, error, options);
+    location = navigator.geolocation.watchPosition(apiGeolocationSuccess, error, options);
 };
 
 $(document).ready(() => {
