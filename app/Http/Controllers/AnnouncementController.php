30ecfd584ad05032e1a4c41450d9d0da0646a947
rival2905 <valdothea@gmail.com>
2021-03-15 14:46:40 +0700

    announce

--- app/Http/Controllers/AnnouncementController.php
+++ app/Http/Controllers/AnnouncementController.php
@@ -77,7 +77,7 @@ class AnnouncementController extends Controller
 
         $title = "Pengumuman ".$request->title;
         $body = $request->content;
-        $userPelapor = DB::table("users")->where('id',Auth::user()->id)->first()->get();
+        $userPelapor = DB::table("users")->where('id',Auth::user()->id)->first();
        
         $users = [$userPelapor];
         
