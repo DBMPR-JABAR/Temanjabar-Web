eae22161efa3a8132b16090f7dc696f8e2d8a513
rival2905 <valdothea@gmail.com>
2021-03-18 12:17:03 +0700

    detail"

--- app/Http/Controllers/DetailUserController.php
+++ app/Http/Controllers/DetailUserController.php
@@ -207,7 +207,7 @@ class DetailUserController extends Controller
             if($request->input('sup_id') != null){
                 $userupdat['sup_id']= $temp[0]; 
                 $userupdat['sup']= $temp[1]; 
-                // dd($temp[0]);
+                dd($temp);
                 $updatetouser = DB::table('users')->where('id', $id)->update($userupdat);
             }
             $updateprofile = DB::table('user_pegawai')
