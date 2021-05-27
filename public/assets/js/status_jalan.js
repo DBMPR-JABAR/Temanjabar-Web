d8de1709371e9b89e7127d34c0c3dbcc47712d00
Firman Mardiyanto <firman.mardiyanto@gmail.com>
2021-05-28 03:30:21 +0700

    sm

--- public/assets/js/status_jalan.js
+++ public/assets/js/status_jalan.js
@@ -1,5 +1,5 @@
-$(document).ready(function() {
-    $('#map_container').ready(() => {
+$(document).ready(function () {
+    $("#map_container").ready(() => {
         require([
             "esri/Map",
             "esri/views/MapView",
@@ -8,77 +8,138 @@ $(document).ready(function() {
             "esri/widgets/Track",
             "esri/widgets/Fullscreen",
             "esri/Graphic",
-        ], function(Map, MapView, BasemapToggle, Compass, Track, Fullscreen, Graphic) {
+            "react",
+            "react-dom",
+            "esri/core/watchUtils",
+            "esri/widgets/Zoom/ZoomViewModel",
+        ], function (
+            Map,
+            MapView,
+            BasemapToggle,
+            Compass,
+            Track,
+            Fullscreen,
+            Graphic,
+            React,
+            ReactDOM,
+            watchUtils,
+            ZoomViewModel
+        ) {
+            const { useState } = React;
+            const StatusJalan = ({ view, track }) => {
+                console.log(view, track);
+                const [coords, setCoords] = useState({
+                    latitude: null,
+                    longitude: null,
+                });
+
+                React.useEffect(() => {
+                    track.on("track", async (trackEvent) => {
+                        const coordsTemp = trackEvent.position.coords;
+                        setCoords({
+                            latitude: coordsTemp.latitude,
+                            longitude: coordsTemp.longitude,
+                        });
+                        // console.log(coords.latitude);
+                    });
+                }, []);
+                console.log(coords);
+                return (
+                    <div className="p-2 card w-30">
+                        <p>Latitude: {coords.latitude ? coords.latitude : 'inisialisasi..'}</p>
+                        <p>Longitude: {coords.longitude ? coords.longitude : 'inisialisasi..'}</p>
+                    </div>
+                );
+            };
 
             const map = new Map({
-                basemap: "osm"
+                basemap: "osm",
             });
 
             const view = new MapView({
                 container: "maps_container",
                 map: map,
-                center: [
-                    107.6191, -6.9175
-                ],
+                center: [107.6191, -6.9175],
                 zoom: 10,
-                sliderPosition: "top-right"
+                ui: {
+                    components: ["attribution"],
+                },
+                sliderPosition: "top-right",
             });
 
             const toggle = new BasemapToggle({
                 view,
-                nextBasemap: "hybrid"
+                nextBasemap: "hybrid",
             });
             const compass = new Compass({
-                view
+                view,
             });
             const track = new Track({
-                view
+                view,
             });
             const fullscreen = new Fullscreen({
-                view
+                view,
             });
-
-            view.ui.add([{
-                component: toggle,
-                position: "bottom-left"
-            }, {
-                component: compass,
-                position: "top-left"
-            }, {
-                component: fullscreen,
-                position: "top-left"
-            }, {
-                component: track,
-                position: "top-left"
-            }]);
+            const statusJalanContainer = document.createElement("div");
+            view.ui.add([
+                {
+                    component: statusJalanContainer,
+                    position: "top-right",
+                },
+                {
+                    component: toggle,
+                    position: "bottom-left",
+                },
+                {
+                    component: compass,
+                    position: "top-left",
+                },
+                {
+                    component: fullscreen,
+                    position: "top-left",
+                },
+                {
+                    component: track,
+                    position: "top-left",
+                },
+            ]);
+            ReactDOM.render(
+                <StatusJalan view={view} track={track} />,
+                statusJalanContainer
+            );
 
             let coords = {
                 latitude,
-                longitude
+                longitude,
             };
 
-            view.when(function() {
+            view.when(function () {
                 track.start();
-                track.on("track", async(trackEvent) => {
+                track.on("track", async (trackEvent) => {
                     const coordsTemp = trackEvent.position.coords;
-                    coords.latitude = coordsTemp.latitude
-                    coords.longitude = coordsTemp.longitude
-                    $('#latitude').text(coordsTemp.latitude)
-                    $('#longitude').text(coordsTemp.longitude)
-                })
+                    coords.latitude = coordsTemp.latitude;
+                    coords.longitude = coordsTemp.longitude;
+                    $("#latitude").text(coordsTemp.latitude);
+                    $("#longitude").text(coordsTemp.longitude);
+                });
                 getData();
             });
 
             const getData = () => {
-                const url = new URL(baseUrl + '/status_jalan/api'),
-                    params = {...coords, radius: 100 }
-                Object.keys(params).forEach(key => url.searchParams.append(key, params[key]))
+                const url = new URL(baseUrl + "/status_jalan/api"),
+                    params = {
+                        ...coords,
+                        radius: 100,
+                    };
+                Object.keys(params).forEach((key) =>
+                    url.searchParams.append(key, params[key])
+                );
                 fetch(url)
                     .then((response) => response.json())
                     .then((data) => {
-                        console.log(data.pemeliharaan)
-                        let html = ''
-                        data.pemeliharaan.forEach(pemeliharaan => {
+                        console.log(data.pemeliharaan);
+                        let html = "";
+                        data.pemeliharaan.forEach((pemeliharaan) => {
                             html += `
                                 <a href="#" class="list-group-item list-group-item-action flex-column align-items-start">
                                   <div class="d-flex w-100 justify-content-between">
@@ -87,13 +148,15 @@ $(document).ready(function() {
                                   </div>
                                   <p class="mb-1">${pemeliharaan.ruas_jalan}</p>
                                   <small>${pemeliharaan.tanggal}</small>
-                                </a>`
-                        })
-                        const pekerjaanContainer = document.getElementById('pekerjaan_list_container')
-                        pekerjaanContainer.innerHTML = html
-                    })
-            }
+                                </a>`;
+                        });
+                        const pekerjaanContainer = document.getElementById(
+                            "pekerjaan_list_container"
+                        );
+                        pekerjaanContainer.innerHTML = html;
+                    });
+            };
         });
     });
-    console.log(geoServerUrl, baseUrl)
+    console.log(geoServerUrl, baseUrl);
 });
--- resources/views/landing/status_jalan/index.blade.php
+++ resources/views/landing/status_jalan/index.blade.php
@@ -8,6 +8,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
     <link rel="stylesheet" href="{{ asset('assets/css/status_jalan.css') }}">
+    <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
     <link rel="stylesheet" href="https://js.arcgis.com/4.19/esri/themes/light/main.css">
     <meta name="csrf-token" content="{{ csrf_token() }}" />
     <title>Status Jalan</title>
@@ -18,40 +19,31 @@
         <img width="200" class="img-fluid" src="{{ asset('assets/images/brand/text_putih.png')}}" alt="Logo DBMPR">
     </div>
     <div id="maps_container"></div>
-    <div id="feature_node" class="esri-widget overflow-auto">
-        <div class="row g-3 w-100">
-            <div class="col-12 col-sm-12 col-md-6 col-xl-4">
-                <div class="card">
-                    <div class="card-body">
-                        <h5 class="card-title">Status Jalan</h5>
-                        <h6 class="card-subtitle mb-2 text-muted">Informasi Posisi Anda</h6>
-                        <p class="card-text">Latitude : <span id="latitude"></span> Longitude : <span
-                                id="longitude"></span></p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-12 col-sm-12 col-md-6 col-xl-4">
-                <div class="card">
-                    <div class="card-body">
-                        <h5 class="card-title">Pekerjaan</h5>
-                        <h6 class="card-subtitle mb-2 text-muted">Lokasi pekerjaan terdekat</h6>
-                        <div class="list-group" id="pekerjaan_list_container">
-                          </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
 </body>
-<script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js"
     integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous">
 </script>
-<script src="https://js.arcgis.com/4.19"></script>
 <script>
     const baseUrl = "{{ url('/') }}";
     const geoServerUrl = "{{ env('GEOSERVER') }}";
+    window.dojoConfig = {
+    async: true,
+    packages: [
+        {
+            name: "react",
+            location: "https://unpkg.com/react@16/umd/",
+            main: "react.development",
+        },
+        {
+            name: "react-dom",
+            location: "https://unpkg.com/react-dom@16/umd/",
+            main: "react-dom.development",
+        }
+    ]
+    };
 </script>
-<script type="text/javascript" src="{{ asset('assets/js/status_jalan.js') }}"></script>
-
+{{-- <script src="https://ajax.googleapis.com/ajax/libs/dojo/1.10.4/dojo/dojo.js"></script> --}}
+<script src="https://unpkg.com/@babel/standalone/babel.min.js"></script>
+<script src="https://js.arcgis.com/4.19"></script>
+<script type="text/babel" src="{{ asset('assets/js/status_jalan.js') }}"></script>
 </html>
