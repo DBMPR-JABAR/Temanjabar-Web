4ab04d50ebad054c3902772d73012d6c3a55374d
rival2905 <valdothea@gmail.com>
2021-09-07 10:28:59 +0700

    tiny

--- resources/views/admin/pengumuman/insert.blade.php
+++ resources/views/admin/pengumuman/insert.blade.php
@@ -138,19 +138,20 @@
 
     <script>
     </script>
-    <script src="//cdn.tinymce.com/4/tinymce.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/tinymce/5.6.2/tinymce.min.js"></script>
     <script>
-        var editor_config = {
-            selector: "textarea.content",
-            plugins: [
-                "advlist autolink lists link image charmap print preview hr anchor pagebreak",
-                "searchreplace wordcount visualblocks visualchars code fullscreen",
-                "insertdatetime media nonbreaking save table contextmenu directionality",
-                "emoticons template paste textcolor colorpicker textpattern"
-            ],
-            toolbar: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image media",
-            relative_urls: false,
-        };
-        tinymce.init(editor_config);
+         var editor_config = {
+         selector: "textarea.content",
+         plugins: [
+                    "advlist autolink lists link image charmap print preview hr anchor pagebreak",
+                    "searchreplace wordcount visualblocks visualchars code fullscreen",
+                    "insertdatetime media nonbreaking save table contextmenu directionality",
+                    "emoticons template paste textcolor colorpicker textpattern"
+                ],
+                toolbar: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image media",
+                relative_urls: false,
+          };
+    
+          tinymce.init(editor_config);
     </script>
 @endsection
