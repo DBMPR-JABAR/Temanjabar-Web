8d3aad1e38efe5c02aa5db2e6ae95e54a9dc9e5a
Firman Mardiyanto <firman.mardiyanto@gmail.com>
2021-09-27 10:00:14 +0700

    show password

--- resources/views/landing/login.blade.php
+++ resources/views/landing/login.blade.php
@@ -1,52 +1,57 @@
 @extends('landing.template')
 @section('body')
 
-    <!-- sign-in -->
-    <section id="sign-in" class="bglight position-relative padding">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-6 col-md-6 rp-0">
-                    <div class="image login-image h-100">
-                        <img src="{{ asset('assets/images/slider/hero01.jpg') }}" alt="" class="w-100 h-100"
-                            style="object-fit: cover;">
-                    </div>
+<!-- sign-in -->
+<section id="sign-in" class="bglight position-relative padding">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6 col-md-6 rp-0">
+                <div class="image login-image h-100">
+                    <img src="{{ asset('assets/images/slider/hero01.jpg') }}" alt="" class="w-100 h-100"
+                        style="object-fit: cover;">
                 </div>
-                <div class="col-md-6 col-md-6 col-sm-12 whitebox">
-                    <div class="widget logincontainer">
-                        <h3 class="darkcolor bottom35">Login </h3>
-                        @if (Session::has('msg'))
-                            <div class="row">
-                                <div class="col-md-12">
-                                    <div class="alert alert-{{ Session::get('color') }} alert-dismissible fade show"
-                                        role="alert">
-                                        {{ Session::get('msg') }}
-                                        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                                            <span aria-hidden="true">&times;</span>
-                                        </button>
+            </div>
+            <div class="col-md-6 col-sm-12 whitebox">
+                <div class="widget logincontainer">
+                    <h3 class="darkcolor bottom35">Login </h3>
+                    @if (Session::has('msg'))
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="alert alert-{{ Session::get('color') }} alert-dismissible fade show"
+                                role="alert">
+                                {{ Session::get('msg') }}
+                                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                    @endif
+                    <form class="getin_form border-form" id="login" action="{{ url('auth') }}" method="POST">
+                        @csrf
+                        <div class="row">
+                            <div class="col-md-12 col-sm-12">
+                                <div class="form-group bottom35">
+                                    <label for="loginEmail" class="d-none"></label>
+                                    <input name="email" class="form-control" type="text" placeholder="Email/NIP:"
+                                        required id="loginEmail">
+                                </div>
+                            </div>
+                            <div class="col-md-12 col-sm-12">
+                                <div class="form-group bottom35">
+                                    <label for="loginPass" class="d-none"></label>
+                                    <div class="input-group" id="show_hide_password">
+                                        <input name="password" class="form-control" type="password"
+                                            placeholder="Password:" required id="loginPass">
+                                        <div class="input-group-addon">
+                                        <a href=""><i class="fa fa-eye-slash" aria-hidden="true"></i></a>
                                     </div>
                                 </div>
                             </div>
-                        @endif
-                        <form class="getin_form border-form" id="login" action="{{ url('auth') }}" method="POST">
-                            @csrf
-                            <div class="row">
-                                <div class="col-md-12 col-sm-12">
-                                    <div class="form-group bottom35">
-                                        <label for="loginEmail" class="d-none"></label>
-                                        <input name="email" class="form-control" type="text" placeholder="Email/NIP:"
-                                            required id="loginEmail">
-                                    </div>
-                                </div>
-                                <div class="col-md-12 col-sm-12">
-                                    <div class="form-group bottom35">
-                                        <label for="loginPass" class="d-none"></label>
-                                        <input name="password" class="form-control" type="password" placeholder="Password:"
-                                            required id="loginPass">
-                                    </div>
-                                </div>
-                                {{-- <div class="col-md-12 col-sm-12">
+                        </div>
+                        {{-- <div class="col-md-12 col-sm-12">
                         <div class="form-group bottom35">
-                           <div class="form-check text-right">
+                           <div class="text-right form-check">
                               <input class="form-check-input" checked type="checkbox" value="" id="rememberMe">
                               <label class="form-check-label" for="rememberMe">
                                     Keep Me Signed In
@@ -55,18 +60,37 @@
 
                         </div>
                      </div> --}}
-                                <div class="col-sm-12">
-                                    <button type="submit" class="button gradient-btn btnprimary">Login</button>
-                                    {{-- <p class="top20 log-meta"> Don't have an account? <a href="sign-up.html">Sign Up Now</a> </p> --}}
-                                </div>
-                            </div>
-                        </form>
-                    </div>
+                        <div class="col-sm-12">
+                            <button type="submit" class="button gradient-btn btnprimary">Login</button>
+                            {{-- <p class="top20 log-meta"> Don't have an account? <a href="sign-up.html">Sign Up Now</a> </p> --}}
+                        </div>
                 </div>
-
+                </form>
             </div>
         </div>
-    </section>
-    <!-- sign-in ends -->
 
+    </div>
+    </div>
+</section>
+<!-- sign-in ends -->
+
+@endsection
+
+@section('script')
+<script type="text/javascript">
+$(document).ready(function() {
+    $("#show_hide_password a").on('click', function(event) {
+        event.preventDefault();
+        if($('#show_hide_password input').attr("type") == "text"){
+            $('#show_hide_password input').attr('type', 'password');
+            $('#show_hide_password i').addClass( "fa-eye-slash" );
+            $('#show_hide_password i').removeClass( "fa-eye" );
+        }else if($('#show_hide_password input').attr("type") == "password"){
+            $('#show_hide_password input').attr('type', 'text');
+            $('#show_hide_password i').removeClass( "fa-eye-slash" );
+            $('#show_hide_password i').addClass( "fa-eye" );
+        }
+    });
+});
+</script>
 @endsection
