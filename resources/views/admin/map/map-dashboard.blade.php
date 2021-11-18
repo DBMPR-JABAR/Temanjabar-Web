8325e91782ba56cad239c3dea13e53e02760fb81
priyayidimas <priyayidimas@upi.edu>
2021-11-18 20:12:00 +0700

    Fix Check

--- resources/views/admin/map/map-dashboard.blade.php
+++ resources/views/admin/map/map-dashboard.blade.php
@@ -11,7 +11,7 @@
     <link rel="icon" href="{{ asset('assets/images/favicon/favicon.ico') }}" type="image/x-icon">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://js.arcgis.com/4.19/esri/themes/light/main.css" />
+    <link rel="stylesheet" href="https://js.arcgis.com/4.20/esri/themes/light/main.css" />
     <link rel="stylesheet" href="{{ asset('assets/css/filterMapsInternal.css') }}" />
     <style>
         html,
