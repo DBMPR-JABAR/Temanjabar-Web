bbee7318468ee2b45221c83a566c780b1f1fd067
rival2905 <valdothea@gmail.com>
2021-12-15 14:44:24 +0700

    user

--- app/Http/Controllers/MasterData/UserController.php
+++ app/Http/Controllers/MasterData/UserController.php
@@ -240,7 +240,7 @@ class UserController extends Controller
 
     public function getUser()
     {
-        $users = DB::table('users')->where('is_delete',null)->Where('is_delete',0)->get();
+        $users = DB::table('users')->where('is_delete',null)->orWhere('is_delete',0)->orWhere('is_delete','!=',1)->get();
         $roles = DB::table('user_role');
         if (Auth::user()->internalRole->uptd) {
             $roles = $roles->where('uptd', Auth::user()->internalRole->uptd);
