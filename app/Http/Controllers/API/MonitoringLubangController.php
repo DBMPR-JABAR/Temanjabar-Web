7c6356fd5f1c2751611f5c29fb75f109a97e1044
DBMPR-JABAR <dbmpr.jabar.prov@gmail.com>
2022-04-19 14:01:13 +0700

    potensi

--- app/Http/Controllers/API/MonitoringLubangController.php
+++ app/Http/Controllers/API/MonitoringLubangController.php
@@ -370,18 +370,16 @@ class MonitoringLubangController extends Controller
                 $this->response['data']['error'] = "Ruas Tidak Ditemukan";
                 return response()->json($this->response, 200);
             }
-            $survei = SurveiLubang::where([
+            $find = [
                 ['tanggal', $request->tanggal],
-                ['created_by' ,Auth::user()->id],
                 ['ruas_jalan_id',$request->ruas_jalan_id],
                 ['sup_id',$ruas->data_sup->id]
-            ])->first();
-            $potensi = SurveiPotensiLubang::where([
-                ['tanggal', $request->tanggal],
-                ['created_by' ,Auth::user()->id],
-                ['ruas_jalan_id',$request->ruas_jalan_id],
-                ['sup_id',$ruas->data_sup->id]
-            ])->first();
+            ];
+            if(Auth::user()->id !=1){
+                $find['created_by'] = Auth::user()->id;
+            }
+            $survei = SurveiLubang::where($find)->first();
+            $potensi = SurveiPotensiLubang::where($find)->first();
             if(isset($survei)){
                 $survei->jumlah = $survei->SurveiLubangDetail->sum('jumlah');
                 $survei->ruas = $survei->ruas()->select('id_ruas_jalan','nama_ruas_jalan')->get();
