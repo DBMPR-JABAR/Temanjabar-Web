071bb152d31397ae4099f0a10b7d5ab74cc5d3ff
DBMPR-JABAR <dbmpr.jabar.prov@gmail.com>
2022-05-25 17:13:04 +0700

    lravel

--- app/Http/Controllers/API/MonitoringLubangController.php
+++ app/Http/Controllers/API/MonitoringLubangController.php
@@ -604,7 +604,7 @@ class MonitoringLubangController extends Controller
             $meters = $kilometers * 1000;
 
             if($data){
-                if($meters <=3){
+                if($meters <= 15){
                     $data->update($temp);
                     $temp = [
                         'tanggal' => $tanggal,
