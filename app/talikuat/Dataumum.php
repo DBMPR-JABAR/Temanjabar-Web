5b25e57a9ec4e8adc1afd51e717525605dacda45
xart22 <fajar.rn22@gmail.com>
2022-05-30 04:36:11 +0700

    typo

--- app/talikuat/Dataumum.php
+++ app/talikuat/Dataumum.php
@@ -5,7 +5,7 @@ namespace App\talikuat;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Dataumum extends Model
+class DataUmum extends Model
 {
     use HasFactory;
     protected $connection = 'talikuat';
