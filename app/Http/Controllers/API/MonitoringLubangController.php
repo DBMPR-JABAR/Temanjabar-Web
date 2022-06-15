e5aa0165277f0fe2e54a5d1f1b312b8f3fede8ed
rival2905 <valdothea@gmail.com>
2022-06-15 11:16:11 +0700

    ruas

--- app/Http/Controllers/API/MonitoringLubangController.php
+++ app/Http/Controllers/API/MonitoringLubangController.php
@@ -946,8 +946,8 @@ class MonitoringLubangController extends Controller
                 'ruas_jalan_id' => $request->ruas_jalan_id
 
             ];
-            $data = SurveiLubangDetail::where('ruas_jalan_id',$request->ruas_jalan_id)->with('user_create')->where('tanggal','<=',$request->tanggal)->whereNull('status')->orderBy("lokasi_km")->orderBy("lokasi_m")->get();
-            $data1 = SurveiLubangDetail::where('ruas_jalan_id',$request->ruas_jalan_id)->with('user_create')->where('tanggal','<=',$request->tanggal)->where('status','Perencanaan')->latest('updated_at')->get();
+            $data = SurveiLubangDetail::where('ruas_jalan_id',$request->ruas_jalan_id)->with('user_create')->with('ruas')->where('tanggal','<=',$request->tanggal)->whereNull('status')->orderBy("lokasi_km")->orderBy("lokasi_m")->get();
+            $data1 = SurveiLubangDetail::where('ruas_jalan_id',$request->ruas_jalan_id)->with('user_create')->with('ruas')->where('tanggal','<=',$request->tanggal)->where('status','Perencanaan')->latest('updated_at')->get();
             // ->with(['user_create' => function ($query) {
             //     $query->select('id', 'username');
             // }])
