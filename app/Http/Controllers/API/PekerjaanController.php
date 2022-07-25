1c0341d43416b7cfe732dce8bfa31cd5b5fa76ba
DBMPR-JABAR <dbmpr.jabar.prov@gmail.com>
2022-07-25 11:13:05 +0700

    store pemeliharaan

--- app/Http/Controllers/API/PekerjaanController.php
+++ app/Http/Controllers/API/PekerjaanController.php
@@ -10,6 +10,8 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Str;
 use App\ItemPeralatan;
+use App\User;
+use App\Model\Transactional\PekerjaanPemeliharaan as Pemeliharaan;
 
 class PekerjaanController extends Controller
 {
@@ -57,96 +59,115 @@ class PekerjaanController extends Controller
     public function store(Request $request)
     {
         try {
+            $pekerjaan = [];
+
             $validator = Validator::make($request->all(), [
-                'tanggal' => 'required|date',
-                'idSup' => 'required',
-                'namaPaket' => 'required|string',
-                'idRuasJalan' => 'required',
-                // 'idJenisPekerjaan' => 'required',
-                'lokasi' => 'required|string|min:3',
-                'lat' => 'required|string',
-                'long' => 'required|string',
-                'panjang' => 'required|string',
-                // 'perkiraan_kuantitas' => 'required',
-                'jumlah_pekerja' => 'required',
-
-                // 'peralatan' => 'required|string',
-                'fotoAwal' => 'file',
-                'fotoSedang' => 'file',
-                'fotoAkhir' => 'file',
-                'fotoPegawai' => 'file',
-                'video' => 'mimes:mp4'
+                'sub_kegiatan' => 'required',
+                'uptd_id' => 'required',
+                'sup_id' => 'required',
+                'ruas_jalan_id' => 'required',
+                'mandor_id' => '',
+                'jenis_pekerjaan_id' => 'required',
+                'kegiatan_pekerjaan_id' => 'required',
+                'tanggal' => 'required',
+                'lokasi' => 'required',
+                'lat' => 'required',
+                'long' => 'required',
+                'panjang'=> 'required',
+                'perkiraan_kuantitas'=> 'required',
+                'foto_awal' => 'required',
+                'foto_sedang' => '',
+                'foto_akhir' => '',
+                'foto_pegawai' => '',
+                'video' => 'mimetypes:video/x-ms-asf,video/x-flv,video/mp4,application/x-mpegURL,video/MP2T,video/3gpp,video/quicktime,video/x-msvideo,video/x-ms-wmv,video/avi|max:102400',
+                'keterangan' =>''
             ]);
-
+            
+            if($this->user->internalRole->role != null && str_contains($this->user->internalRole->role,'Mandor')){
+                $pekerjaan['nama_mandor'] = $this->user->name;
+                $pekerjaan['user_id'] =$this->user->id;
+            }else{
+                $validator = Validator::make($request->all(), [
+                    'nama_mandor' => 'required',
+                ]);
+                $user = User::find($request->mandor_id);
+                $pekerjaan['nama_mandor'] = $user->name;
+                $pekerjaan['user_id'] = $user->id;
+            }
             if ($validator->fails()) {
                 $this->response['data']['error'] = $validator->errors();
                 return response()->json($this->response, 200);
             }
+            $pekerjaan['sub_kegiatan'] = $request->sub_kegiatan;
+            
+            $pekerjaan['uptd_id'] = $request->uptd_id;
+            $sup = DB::table('utils_sup')->find($request->sup_id);
+            $pekerjaan['sup_id'] = $sup->id;
+            $pekerjaan['sup'] = $sup->name;
+            $ruas = DB::table('master_ruas_jalan')->where('id_ruas_jalan',$request->ruas_jalan_id)->first();
+            $pekerjaan['ruas_jalan'] = $ruas->nama_ruas_jalan;
+            $pekerjaan['ruas_jalan_id'] = $ruas->id_ruas_jalan;
+            
+            $pekerjaan['jenis_pekerjaan'] = $request->jenis_pekerjaan_id;
 
-            $pekerjaan = [];
-            $pekerjaan['nama_mandor'] = $this->user->name;
-            $pekerjaan['user_id'] = $this->user->id;
-            $pekerjaan['created_by'] = $this->user->id;
+            $nama_kegiatan_pekerjaan = DB::table('utils_nama_kegiatan_pekerjaan')->find($request->kegiatan_pekerjaan_id);
+            $pekerjaan['paket'] = $nama_kegiatan_pekerjaan->name;
+            $pekerjaan['kegiatan_pekerjaan_id'] = $nama_kegiatan_pekerjaan->id;
+            
             $pekerjaan['tanggal'] = $request->tanggal;
-            $pekerjaan['sup_id'] = $request->idSup;
-            $pekerjaan['sup'] = DB::table('utils_sup')->where('id', $request->idSup)->first()->name;
-            $pekerjaan['paket'] = $request->namaPaket;
-            $pekerjaan['ruas_jalan_id'] = $request->idRuasJalan;
-            $pekerjaan['ruas_jalan'] = DB::table('master_ruas_jalan')
-                ->where('id_ruas_jalan', $request->idRuasJalan)->first()->nama_ruas_jalan;
-            // $pekerjaan['jenis_pekerjaan'] = DB::table('item_pekerjaan')->where('no', $request->idJenisPekerjaan)->first()->nama_item;
-            $pekerjaan['jenis_pekerjaan'] = "Pemeliharaan";
-            // $pekerjaan['peralatan'] = $request->peralatan;
-            $pekerjaan['panjang'] = $request->panjang;
-            // $pekerjaan['perkiraan_kuantitas'] = $request->perkiraan_kuantitas;
-            $pekerjaan['jumlah_pekerja'] = $request->jumlah_pekerja;
-
             $pekerjaan['lokasi'] = $request->lokasi;
 
             $pekerjaan['lat'] = $request->lat;
             $pekerjaan['lng'] = $request->long;
-
-            if ($request->fotoAwal != null) {
-                $path = Str::snake(date("YmdHis") . ' ' . $request->fotoAwal->getClientOriginalName());
-                $request->fotoAwal->storeAs('public/pekerjaan/', $path);
+            $pekerjaan['panjang'] = $request->panjang;
+            $pekerjaan['perkiraan_kuantitas'] = $request->perkiraan_kuantitas;
+            
+            if ($request->foto_awal != null) {
+                $path = Str::snake(date("YmdHis") . ' ' . uniqid());
+                $request->foto_awal->storeAs('public/pekerjaan/', $path);
                 $pekerjaan['foto_awal'] = $path;
             }
-            if ($request->fotoSedang != null) {
-                $path = Str::snake(date("YmdHis") . ' ' . $request->fotoSedang->getClientOriginalName());
-                $request->fotoSedang->storeAs('public/pekerjaan/', $path);
+            if ($request->foto_sedang != null) {
+                $path = Str::snake(date("YmdHis") . ' ' .uniqid());
+                $request->foto_sedang->storeAs('public/pekerjaan/', $path);
                 $pekerjaan['foto_sedang'] = $path;
             }
-            if ($request->fotoAkhir != null) {
-                $path = Str::snake(date("YmdHis") . ' ' . $request->fotoAkhir->getClientOriginalName());
-                $request->fotoAkhir->storeAs('public/pekerjaan/', $path);
+            if ($request->foto_akhir != null) {
+                $path = Str::snake(date("YmdHis") . ' ' . uniqid());
+                $request->foto_akhir->storeAs('public/pekerjaan/', $path);
                 $pekerjaan['foto_akhir'] = $path;
             }
-            if ($request->fotoPegawai != null) {
-                $path = Str::snake(date("YmdHis") . ' ' . $request->fotoPegawai->getClientOriginalName());
-                $request->fotoPegawai->storeAs('public/pekerjaan/', $path);
+            if ($request->foto_pegawai != null) {
+                $path = Str::snake(date("YmdHis") . ' ' . uniqid());
+                $request->foto_pegawai->storeAs('public/pekerjaan/', $path);
                 $pekerjaan['foto_pegawai'] = $path;
             }
             if ($request->video != null) {
-                $path = Str::snake(date("YmdHis") . ' ' . $request->video->getClientOriginalName());
+                $path = Str::snake(date("YmdHis") . ' ' .uniqid());
                 $request->video->storeAs('public/pekerjaan/', $path);
                 $pekerjaan['video'] = $path;
             }
 
             $row = DB::table('kemandoran')->select('id_pek')->orderByDesc('id_pek')->limit(1)->first();
+            if($row){
 
-            $pekerjaan['uptd_id'] = $this->userUptd == '' ? 0 : $this->userUptd;
+                $nomor = intval(substr($row->id_pek, strlen('CK-'))) + 1;
+            }else
+                $nomor = 000001;
+
+                
+            $pekerjaan['id_pek'] = 'CK-' . str_pad($nomor, 6, "0", STR_PAD_LEFT);
+            $pekerjaan['created_by'] = $this->user->id;
             $pekerjaan['tglreal'] = date('Y-m-d H:i:s');
             $pekerjaan['is_deleted'] = 0;
-            $nomor = intval(substr($row->id_pek, strlen('CK-'))) + 1;
-            $pekerjaan['id_pek'] = 'CK-' . str_pad($nomor, 6, "0", STR_PAD_LEFT);
 
-            DB::table('kemandoran')->insert($pekerjaan);
-            // DB::table('utils_pekerjaan')->insert([
-            //     'id_mandor' => $this->user->id,
-            //     'id_pek' => $pekerjaan['id_pek']
-            // ]);
-            $this->response['status'] = 'success';
-            $this->response['data']['message'] = 'Berhasil menambahkan pekerjaan';
+            Pemeliharaan::create($pekerjaan);
+            storeLogActivity(declarLog(1, 'Pemeliharaan Pekerjaan', $pekerjaan['id_pek'], 1 ));
+            return response()->json([
+                'success' => true,
+                'message' => 'Berhasil menambahkan pekerjaan Pemeliharaan',
+                'data' => $pekerjaan
+            ]);
             return response()->json($this->response, 200);
         } catch (\Exception $th) {
             $this->response['data']['message'] = 'Internal Error' . $th;
